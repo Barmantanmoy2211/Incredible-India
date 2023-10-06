68f29fcf0534469f487e02340d5a9a1e835f58c7
Vedant Pandey <vedantpandey459@gmail.com>
2023-10-06 22:11:41 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="container mx-auto bg-[#f9fafb] max-w-full bg-opacity-75">
-      <div className="flex flex-col md:flex-row items-center justify-between text-black-500 h-20">
+    <footer className="container mx-auto bg-[#f9fafb] max-w-full bg-opacity-75 grid">
+      <div className="flex flex-col md:flex-row items-center justify-between text-black-500 h-20 ">
         <p className="mb-4 md:mb-0 md:text-xl mx-96">Travel with Us</p>
 
         <div className="flex mx-96">
@@ -21,7 +21,7 @@ function Footer() {
           </a>
         </div>
       </div>
-      <div className=" bg-gradient-to-r from-purple-500 to-pink-500 h-12 max-w-full flex items-center justify-center">
+      <div className=" bg-gradient-to-r from-purple-500 to-pink-500 h-12 max-w-full flex items-center justify-center my-2">
         <p className="text-white items-center text-lg text-center">
           &copy; {new Date().getFullYear()} Indian Heritage and Culture
         </p>
